3587327c34079bd891e7f1413b7b7fc88823244c
Hyoun Jun (이현준) <98198915+Koalajuni@users.noreply.github.com>
2024-07-01 09:49:47 +0900

    Update dbadd.py

--- openai_mariaDB_project/dbadd.py
+++ openai_mariaDB_project/dbadd.py
@@ -10,8 +10,8 @@ def ChatGPTadd(human_chat,ai_chat):
     conn = pymysql.connect(
         host= '127.0.0.1',
         user='chat',
-        password='@1234@',
-        database='chatdb', 
+        password=PSSWRD,
+        database=DBKEY, 
         port = 3306,
         charset='utf8',
     )
